60754c1b38870142cb85d43cdbddf311c192daab
gledson.cruz@gmail.com <gledson.cruz@gmail.com@53dddd47-af53-4b5c-4b2f-23ebcaace975>
2011-03-11 20:52:16 +0000

    Adicionando CRUD de Status de Serviços
    git-svn-id: http://sisos.googlecode.com/svn/trunk/sisOS@31 53dddd47-af53-4b5c-4b2f-23ebcaace975

---
+++ src/hot/br/com/sisos/action/ServicoStatusAction.java
@@ -0,0 +1,55 @@
+package br.com.sisos.action;
+
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Begin;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Out;
+import org.jboss.seam.annotations.Scope;
+
+import br.com.sisos.entity.ServicoStatus;
+import br.com.sisos.services.ServicoStatusService;
+
+
+@Name(value = "servicoStatusAction")
+@Scope(ScopeType.CONVERSATION)
+public class ServicoStatusAction extends BaseAction{
+
+	private static final long serialVersionUID = 1L;
+	
+
+	@In(create = true)
+	@Out(scope = ScopeType.CONVERSATION, required = false)
+	private ServicoStatus servicoStatus;
+
+	@In
+	private ServicoStatusService servicoStatusService;
+
+	public ServicoStatus getServicoStatus() {
+		return servicoStatus;
+	}
+
+	public void setServicoStatus(ServicoStatus servicoStatus) {
+		this.servicoStatus = servicoStatus;
+	}
+	
+	@Begin (join = true)
+	public void salvar(ServicoStatus servicoStatus){
+		
+		this.servicoStatusService.salvar(servicoStatus);
+		this.servicoStatus = new ServicoStatus();
+		this.addMsgBundle(FacesMessage.SEVERITY_INFO, "crudSaveSucess");
+		
+	}
+	
+	public List<ServicoStatus> carregarTodos(){
+		
+		return servicoStatusService.carregarTodos();
+		
+	}
+
+}
