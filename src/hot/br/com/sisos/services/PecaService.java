c1bc5363ce986e7b22489587e0893316a8033b43
gledson.cruz@gmail.com <gledson.cruz@gmail.com@53dddd47-af53-4b5c-4b2f-23ebcaace975>
2011-03-14 22:33:28 +0000

    Adicionando services para pecas e tipos de servicos
    git-svn-id: http://sisos.googlecode.com/svn/trunk/sisOS@40 53dddd47-af53-4b5c-4b2f-23ebcaace975

---
+++ src/hot/br/com/sisos/services/PecaService.java
@@ -0,0 +1,51 @@
+package br.com.sisos.services;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Order;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+import br.com.sisos.entity.Peca;
+
+@Name("pecaService")
+@Scope(ScopeType.STATELESS)
+@AutoCreate
+public class PecaService extends BaseService implements BaseInterfaceService<Peca, Serializable>{
+
+	private static final long serialVersionUID = 1L;
+
+	public void alterar(Peca obj) {
+		hibernateSession.update(obj);
+		
+	}
+
+	public Peca carregarPorId(Integer id) {
+		return (Peca) hibernateSession.load(Peca.class, id);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Peca> carregarTodos() {
+		Criteria criteria = hibernateSession.createCriteria(Peca.class);
+		criteria.addOrder(Order.asc("descricao"));
+		return criteria.list();
+	}
+
+	public void excluir(Peca obj) {
+		hibernateSession.delete(obj);
+		
+	}
+
+	public void salvar(Peca obj) {
+		hibernateSession.save(obj);
+		
+	}
+	
+	
+	
+
+}
--- src/hot/br/com/sisos/services/TipoServicoService.java
+++ src/hot/br/com/sisos/services/TipoServicoService.java
@@ -1,5 +1,51 @@
 package br.com.sisos.services;
 
-public class TipoServicoService {
+import java.io.Serializable;
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Order;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+import br.com.sisos.entity.TipoServico;
+
+@Name("tipoServicoService")
+@Scope(ScopeType.STATELESS)
+@AutoCreate
+public class TipoServicoService extends BaseService implements BaseInterfaceService<TipoServico, Serializable>{
+
+	private static final long serialVersionUID = 1L;
+
+	public void alterar(TipoServico obj) {
+		hibernateSession.update(obj);
+		
+	}
+
+	public TipoServico carregarPorId(Integer id) {
+		return (TipoServico) hibernateSession.load(TipoServico.class, id);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<TipoServico> carregarTodos() {
+		Criteria criteria = hibernateSession.createCriteria(TipoServico.class);
+		criteria.addOrder(Order.asc("descricao"));
+		return criteria.list();
+	}
+
+	public void excluir(TipoServico obj) {
+		this.hibernateSession.delete(obj);
+		
+	}
+
+	public void salvar(TipoServico obj) {
+		this.hibernateSession.save(obj);
+		
+	}
+	
+	
+	
 
 }
