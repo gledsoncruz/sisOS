6c2a510983679a2ce53d992c5c71f0277a4d9f6e
gledson.cruz@gmail.com <gledson.cruz@gmail.com@53dddd47-af53-4b5c-4b2f-23ebcaace975>
2011-03-28 01:09:30 +0000

    Criando um metodo de consulta com like de peças
    git-svn-id: http://sisos.googlecode.com/svn/trunk/sisOS@55 53dddd47-af53-4b5c-4b2f-23ebcaace975

--- src/hot/br/com/sisos/services/PecaService.java
+++ src/hot/br/com/sisos/services/PecaService.java
@@ -4,7 +4,9 @@ import java.io.Serializable;
 import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
@@ -45,7 +47,12 @@ public class PecaService extends BaseService implements BaseInterfaceService<Pec
 		
 	}
 	
-	
+	@SuppressWarnings("unchecked")
+	public List<Peca> carregarPecaLike(String param) {
+		Criteria criteria = hibernateSession.createCriteria(Peca.class);
+		criteria.add(Restrictions.like("descricao", param, MatchMode.START).ignoreCase());
+		return criteria.list();
+	}
 	
 
 }
